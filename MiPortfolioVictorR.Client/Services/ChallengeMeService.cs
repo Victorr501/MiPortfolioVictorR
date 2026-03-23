83f087d4507446af133732af8b669356d6bd1bf5
Víctor Rubín Rubio <VrubinR501@outlook.com>
2026-03-23 23:07:34 +0100

    actualizacion final 23/03/2026

--- MiPortfolioVictorR.Client/Services/ChallengeMeService.cs
+++ MiPortfolioVictorR.Client/Services/ChallengeMeService.cs
@@ -6,7 +6,13 @@ namespace MiPortfolioVictorR.Client.Services
     {
         public List<PfcUpdate> GetUpdates() =>
         [
-            
+            new PfcUpdate
+            {
+                Date        = new DateOnly(2026, 03, 23),
+                Title       = "Incio de proyecto",
+                Description = "Hoy he empezado este nuevo proyecto, lo primero que he hecho ha sido encontrar la idea del proyecto, ver si es economicamente rentable para poder despleg" +
+                "arlo y ver la arquitectura de la aplicacion"
+            },
         ];
     }
 }
