dd8e36c7e8a0a21205894297526fa5a8ee904ea4
Víctor Rubín Rubio <VrubinR501@outlook.com>
2026-04-22 19:58:13 +0200

    Actualizacion 22/04/2026 y orden de mostrar como chalengMe!

--- MiPortfolioVictorR.Client/Services/ChallengeMeService.cs
+++ MiPortfolioVictorR.Client/Services/ChallengeMeService.cs
@@ -8,18 +8,31 @@ namespace MiPortfolioVictorR.Client.Services
         [
             new PfcUpdate
             {
-                Date        = new DateOnly(2026, 03, 23),
-                Title       = "Incio de proyecto",
-                Description = "Hoy he empezado este nuevo proyecto, lo primero que he hecho ha sido encontrar la idea del proyecto, ver si es economicamente rentable para poder despleg" +
-                "arlo y ver la arquitectura de la aplicacion"
+                Date = new DateOnly(2026, 04, 22),
+                Title = "Desarrollo de login en Android, diseño de logo y planificación de escalabilidad",
+                Description = "Ya he desarrollado el login y registro en ChallengMe para Android usando Kotlin. Además, he terminado de diseñar el logo de la aplicación. Los siguientes pasos serán integrar este logo en las interfaces, investigar el proceso de publicación en la AppStore y PlayStore, y desarrollar los tests de la API, una tarea invisible para el usuario pero fundamental para asegurar la escalabilidad del proyecto."
             },
             new PfcUpdate
             {
-                Date        = new DateOnly(2026, 03, 29),
-                Title       = "Autenticación, middleware y conexión a base de datos",
-                Description = "Esta semana he estructurado los dos flujos de login (email/contraseña y Microsoft), " +
-                              "configurado el JWT, los middlewares de excepciones globales y establecido la conexión " +
-                              "con Azure SQL. Próximos pasos: integrar Azure Entra ID (AD) y conectar todo con el frontend en Blazor."
+                Date = new DateOnly(2026, 04, 19),
+                Title = "Desarrollo inicial de iOS completado y planificación para Android",
+                Description = "Ya he desarrollado el login, registro y la pantalla principal de mi proyecto ChallengMe! en iOS. Está funcionando perfectamente, usando el mismo servidor que la página web. Lo próximo que haré será desarrollar la versión para Android usando Kotlin (manteniendo las mismas funciones), crear el icono de la aplicación y publicar ambas versiones para que las podáis ir descargando y probando."
+            },
+            new PfcUpdate
+            {
+                Date = new DateOnly(2026, 04, 13),
+                Title = "Solucion de la discrepancia del entorno y creacion de la aplicacion para android",
+                Description = "Ya he terminado de solucionar el problema en la autenticacion, al final lo que he tenido que hacer es usar localstorage en vez de la cookes devido a que una vez arrancada la pestaña yo no podia hacer una peticion al servidor ya que el http estaba desabilitado. A ademas he creado ya el repostirio para la aplicacion en Android usando Kotlin"
+            },
+            new PfcUpdate
+            {
+                Date = new DateOnly(2026, 04, 11),
+                Title = "Despliegue en Azure e inicio de desarrollo nativo (iOS y Android)",
+                Description = "Se ha implementado con éxito la autenticación de Microsoft y la configuración de seguridad base. " +
+                            "Actualmente, existe una discrepancia en la persistencia de sesión entre el entorno local y Azure, " +
+                            "impidiendo que el usuario se mantenga logueado tras el despliegue. Se está trabajando en ajustar " +
+                            "la gestión de contextos para producción. Paralelamente, con el fin de optimizar la experiencia móvil, " +
+                            "hemos inicializado los repositorios para las aplicaciones nativas en Swift (iOS) y Kotlin (Android)."
             },
             new PfcUpdate
             {
@@ -31,25 +44,18 @@ namespace MiPortfolioVictorR.Client.Services
             },
             new PfcUpdate
             {
-                Date = new DateOnly(2026, 04, 11),
-                Title = "Despliegue en Azure e inicio de desarrollo nativo (iOS y Android)",
-                Description = "Se ha implementado con éxito la autenticación de Microsoft y la configuración de seguridad base. " +
-                            "Actualmente, existe una discrepancia en la persistencia de sesión entre el entorno local y Azure, " +
-                            "impidiendo que el usuario se mantenga logueado tras el despliegue. Se está trabajando en ajustar " +
-                            "la gestión de contextos para producción. Paralelamente, con el fin de optimizar la experiencia móvil, " +
-                            "hemos inicializado los repositorios para las aplicaciones nativas en Swift (iOS) y Kotlin (Android)."   
+                Date        = new DateOnly(2026, 03, 29),
+                Title       = "Autenticación, middleware y conexión a base de datos",
+                Description = "Esta semana he estructurado los dos flujos de login (email/contraseña y Microsoft), " +
+                              "configurado el JWT, los middlewares de excepciones globales y establecido la conexión " +
+                              "con Azure SQL. Próximos pasos: integrar Azure Entra ID (AD) y conectar todo con el frontend en Blazor."
             },
             new PfcUpdate
             {
-                Date = new DateOnly(2026, 04, 13),
-                Title = "Solucion de la discrepancia del entorno y creacion de la aplicacion para android",
-                Description = "Ya he terminado de solucionar el problema en la autenticacion, al final lo que he tenido que hacer es usar localstorage en vez de la cookes devido a que una vez arrancada la pestaña yo no podia hacer una peticion al servidor ya que el http estaba desabilitado. A ademas he creado ya el repostirio para la aplicacion en Android usando Kotlin"
-            },
-            new PfcUpdate
-            {
-                Date = new DateOnly(2026, 04, 19),
-                Title = "Desarrollo inicial de iOS completado y planificación para Android",
-                Description = "Ya he desarrollado el login, registro y la pantalla principal de mi proyecto ChallengMe! en iOS. Está funcionando perfectamente, usando el mismo servidor que la página web. Lo próximo que haré será desarrollar la versión para Android usando Kotlin (manteniendo las mismas funciones), crear el icono de la aplicación y publicar ambas versiones para que las podáis ir descargando y probando."
+                Date        = new DateOnly(2026, 03, 23),
+                Title       = "Incio de proyecto",
+                Description = "Hoy he empezado este nuevo proyecto, lo primero que he hecho ha sido encontrar la idea del proyecto, ver si es economicamente rentable para poder despleg" +
+                "arlo y ver la arquitectura de la aplicacion"
             }
         ];
     }
